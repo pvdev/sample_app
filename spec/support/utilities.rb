c79288ae68219c1c10ca13cc748f235d5404110d
Perry <perry@ivessels.com>
2012-10-14 21:42:23 -0400

    Finish updating users

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,3 +7,11 @@ RSpec::Matchers.define :have_error_message do |message|
   end
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Support sign-in when not using Capybara.
+  cookies[:remember_token] = user.remember_token
+end
